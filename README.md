c408780dd0b83e75f5cb4967343f2b1ef8c5da91
David Sanders <dpsanders@gmail.com>
2017-01-26 17:36:01 -0500

    Add basic README

--- README.md
+++ README.md
@@ -5,3 +5,29 @@
 [![Coverage Status](https://coveralls.io/repos/dpsanders/TreeView.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/dpsanders/TreeView.jl?branch=master)
 
 [![codecov.io](http://codecov.io/github/dpsanders/TreeView.jl/coverage.svg?branch=master)](http://codecov.io/github/dpsanders/TreeView.jl?branch=master)
+
+
+This is a small package to visualize a graph corresponding to an
+abstract syntax tree (AST) of a Julia expression. It uses the `TikzGraphs.jl`
+package to do the visualization.
+
+## Usage
+
+The package is designed to be used within the IJulia notebook.
+
+The simplest usage is `@tree <EXPRESSION>`, where `<EXPRESSION>` represents any
+Julia expression, for example
+```
+@tree x^2 + y^2
+```
+
+## Author
+
+- [David P. Sanders](http://sistemas.fciencias.unam.mx/~dsanders),
+Departamento de Física, Facultad de Ciencias, Universidad Nacional Autónoma de México (UNAM)
+
+## Acknowledements
+Financial support is acknowledged from DGAPA-UNAM PAPIME grants PE-105911 and PE-107114, and DGAPA-UNAM PAPIIT grant IN-117214, and from a CONACYT-Mexico sabbatical fellowship. The author thanks Alan Edelman and the Julia group for hospitality during his sabbatical visit.
+
+In particular, he thanks Jiahao Chen, who, when asked if this package existed,
+replied "why don't you implement it?".
--- src/TreeView.jl
+++ src/TreeView.jl
@@ -1,5 +1,108 @@
 module TreeView
 
-# package code goes here
+using LightGraphs, TikzGraphs
+using MacroTools
+
+export LabelledTree, walk_tree, walk_tree!, draw, @tree, @tree_with_call,
+        tikz_representation
+
+immutable LabelledTree
+    g::Graph
+    labels::Vector{String}
+end
+
+add_numbered_vertex!(g) = (add_vertex!(g); top = nv(g))  # returns the number of the new vertex
+
+"Convert the current node into a label"
+function label(sym)
+    sym == :(^) && return "\\textasciicircum"  # TikzGraphs chokes on ^
+
+    return string("\\texttt{", sym, "}")
+end
+
+
+"""
+    walk_tree!(g, labels, ex, show_call=true)
+
+Walk the abstract syntax tree (AST) of the given expression `ex`.
+Builds up the graph `g` and the set of `labels`
+for each node, both modified in place
+
+`show_call` specifies whether to include `call` nodes in the graph.
+Including them represents the Julia AST more precisely, but adds visual noise.
+
+Returns the number of the top vertex.
+"""
+
+function walk_tree!(g, labels, ex, show_call=true)
+
+    top_vertex = add_numbered_vertex!(g)
+
+    start_argument = 1  # which argument to start with
+
+    if !(show_call) && ex.head == :call
+        f = ex.args[1]   # the function name
+        push!(labels, label(f))
+
+        start_argument = 2   # drop "call" from tree
+
+    else
+        push!(labels, label(ex.head))
+    end
+
+
+    for i in start_argument:length(ex.args)
+
+        if isa(ex.args[i], Expr)
+
+            child = walk_tree!(g, labels, ex.args[i], show_call)
+            add_edge!(g, top_vertex, child)
+
+        else
+            n = add_numbered_vertex!(g)
+            add_edge!(g, top_vertex, n)
+
+            push!(labels, label(ex.args[i]))
+
+        end
+    end
+
+    return top_vertex
+
+end
+
+function walk_tree(ex::Expr, show_call=false)
+    g = Graph()
+    labels = String[]
+
+    walk_tree!(g, labels, ex, show_call)
+
+    return LabelledTree(g, labels)
+
+end
+
+tikz_representation(tree) = TikzGraphs.plot(tree.g, tree.labels)
+
+import Base.show
+function show(io::IO, mime::MIME"image/svg+xml", tree::LabelledTree)
+    p = tikz_representation(tree)  # TikzPicture object
+    show(io, mime, p)
+end
+
+
+
+function draw(tree::LabelledTree)
+    TikzGraphs.plot(tree.g, tree.labels)
+end
+
+
+macro tree(ex::Expr)
+    walk_tree(ex)
+end
+
+macro tree_with_call(ex::Expr)
+    walk_tree(ex, true)
+end
+
 
 end # module
