9a17fb5909c0eb5c52d8ecc3be9cd717335b36f3
David Sanders <dpsanders@gmail.com>
2017-01-27 05:31:36 -0500

    Add basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,18 @@ using TreeView
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+@testset "@tree" begin
+
+    t = @tree 1x
+
+    @test typeof(t) == TreeView.LabelledTree
+    @test typeof(t.g) == LightGraphs.Graph
+    @test typeof(t.labels) == Vector{String}
+
+    @test t.g.vertices == 1:3
+    @test t.labels == String["\\texttt{*}","\\texttt{1}","\\texttt{x}"]
+
+
+    t = @tree x^2 + y^2
+    @test length(t.labels) == 7
+end
